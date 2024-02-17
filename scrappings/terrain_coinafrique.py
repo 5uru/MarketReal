8338da37f13003e5cb19b5ba0a0688641d34a1a0
Restyled.io <commits@restyled.io>
2024-02-17 18:26:22 +0000

    Restyled by autopep8

--- scrappings/terrain_coinafrique.py
+++ scrappings/terrain_coinafrique.py
@@ -6,7 +6,8 @@ for i in range(119):
     page = requests.get(URL)
     soup = BeautifulSoup(page.content, "html.parser")
     # get url on a "class="card-image ad__card-image waves-block waves-light"
-    results = soup.find_all("a", class_="card-image ad__card-image waves-block waves-light")
+    results = soup.find_all(
+        "a", class_="card-image ad__card-image waves-block waves-light")
 
     # get href on a "class="card-image ad__card-image waves-block waves-light" and save on txt file
     for result in results:
@@ -31,10 +32,12 @@ with open('terrain_coinafrique.txt', 'r') as file:
             title = soup.find("span", class_="breadcrumb cible").text
             price = soup.find("p", class_="price").text
             superficie = soup.find("span", class_="qt").text
-            description = soup.find("div", class_="ad__info__box ad__info__box-descriptions").text
+            description = soup.find(
+                "div", class_="ad__info__box ad__info__box-descriptions").text
             # save on csv file
             with open('terrain_coinafrique.csv', 'a') as file_csv:
-                file_csv.write(f"{title}, {price}, {superficie}, {description}\n")
+                file_csv.write(
+                    f"{title}, {price}, {superficie}, {description}\n")
         except Exception:
             print("An error occurred")
             continue
